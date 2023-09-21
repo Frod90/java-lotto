600d7e1bda6ab0c43a3a2668f8bc32332e8ebe77
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-09-22 07:35:15 +0900

    Feat: 로또 구매 금액 입력 기능 구현
    로또 구매 금액을 입력하면 구매한 로또 개수를 알려주는 기능과 1000원 단위 이외의 금액을 기입하면 예외처리 되는 매서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -1,7 +1,30 @@
 package lotto;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
-    public static void main(String[] args) {
-        // TODO: 프로그램 구현
-    }
+	public static void main(String[] args) {
+		System.out.printf("구입금액을 입력해 주세요.%n");
+		String purchaseAmount = Console.readLine();
+
+		Input userInput = new Input();
+
+		int userPurchaseTime = userInput.purchaseTime(purchaseAmount);
+
+	}
+}
+
+class Input {
+	final int lottoPrice = 1000;
+
+	int purchaseTime(String purchaseAmount) {
+		int userPurchaseAmount = Integer.parseInt(purchaseAmount);
+		int PurchaseTest = userPurchaseAmount % lottoPrice;
+
+		if (!(PurchaseTest == 0)) {
+			throw new IllegalArgumentException("[ERROR] 로또는 1000원 단위로 구매 가능합니다.");
+		}
+		return userPurchaseAmount / lottoPrice;
+	}
+
 }
