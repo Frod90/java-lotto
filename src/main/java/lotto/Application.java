a9ea9c11435695f727ad9011164f2a7d9a4767d7
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-09-23 09:54:09 +0900

    Feat: Enum, Hit, resultCount 구현
    당첨 목록인 Enum과 맞춘 로또 번호 개수를 알려주는 Hit 클래스, 당첨 통계를 계산하고 출력하는 resultCount 메서드 구현

--- src/main/java/lotto/Application.java
+++ src/main/java/lotto/Application.java
@@ -7,31 +7,65 @@ import java.util.List;
 import camp.nextstep.edu.missionutils.Console;
 import camp.nextstep.edu.missionutils.Randoms;
 
+enum Prize {
+	FIFTH("5등", "3개 일치", 5000L), FOURTH("4등", "4개 일치", 50000L), THIRD("3등", "5개 일치", 1500000L), SECOND("2등",
+		"5개 일치, 보너스 볼 일치", 30000000L), FIRST("1등", "6개 일치", 2000000000L);
+
+	final String rank;
+	final String hit;
+	final long money;
+
+	Prize(String rank, String hit, long money) {
+		this.rank = rank;
+		this.hit = hit;
+		this.money = money;
+
+	}
+
+}
+
 public class Application {
 	public static void main(String[] args) {
 		System.out.printf("구입금액을 입력해 주세요.%n");
-		String purchaseAmount = Console.readLine();
+		String inuptPurchaseAmount = Console.readLine();
 
 		Purchase purchase = new Purchase();
 
-		int userPurchaseTime = purchase.purchaseTime(purchaseAmount);
-		List<List<Integer>> lottoList = lottoNumber(userPurchaseTime);
+		int PurchaseTime = purchase.purchaseTime(inuptPurchaseAmount);
+		System.out.println();
+		System.out.println(PurchaseTime + "개를 구매했습니다.");
 
-		for (int i = 0; i < userPurchaseTime; i++) {
+		List<List<Integer>> lottoList = lottoNumber(PurchaseTime);
+
+		for (int i = 0; i < PurchaseTime; i++) {
 			System.out.println(lottoList.get(i));
 		}
+
+		System.out.println();
 
 		WinningNumber win = new WinningNumber();
 
 		System.out.printf("당첨 번호를 입력해 주세요.%n");
 		String inputWinningNumber = Console.readLine();
-		List<Integer> winningNumberTest = win.winningNumber(inputWinningNumber);
-		Lotto winningNumber = new Lotto(winningNumberTest);
+		List<Integer> winningNumber = win.winningNumber(inputWinningNumber);
+		new Lotto(winningNumber);
 
+		System.out.println();
 		System.out.printf("보너스 번호를 입력해 주세요.%n");
 		String inputBonusNumber = Console.readLine();
 		int bonusNumber = Integer.parseInt(inputBonusNumber);
 		win.bonusNumberTest(bonusNumber);
+
+		Hit hit = new Hit();
+		List<String> winningList = hit.winningHitList(lottoList, winningNumber, bonusNumber);
+
+		System.out.println();
+		System.out.println("당첨 통계");
+		System.out.println("---");
+
+		Prize[] PRIZE_ARR = Prize.values();
+
+		resultPrint(winningList, PRIZE_ARR, inuptPurchaseAmount);
 
 	}
 
@@ -44,6 +78,35 @@ public class Application {
 		}
 		return lottoList;
 	}
+
+	static int resultCount(List<String> winningList, Prize[] PRIZE_ARR, int winningIndex, int prizeIndex, int count) {
+
+		if (winningList.get(winningIndex).equals(PRIZE_ARR[prizeIndex].hit)) {
+			++count;
+		}
+		return count;
+	}
+
+	static void resultPrint(List<String> winningList, Prize[] PRIZE_ARR, String inputPurchaseAmount) {
+
+		double sum = 0;
+
+		for (int i = 0; i < PRIZE_ARR.length; i++) {
+			int resultCount = 0;
+			for (int j = 0; j < winningList.size(); j++) {
+				resultCount = resultCount(winningList, PRIZE_ARR, j, i, resultCount);
+			}
+			sum = sum + PRIZE_ARR[i].money * resultCount;
+			System.out.printf("%s (%,d원) - %d개%n", PRIZE_ARR[i].hit, PRIZE_ARR[i].money, resultCount);
+
+		}
+
+		double purchaseAmount = Double.parseDouble(inputPurchaseAmount);
+		double summary = sum / purchaseAmount;
+		System.out.printf("총 수익률은 %.1f%%입니다.", summary * 100);
+
+	}
+
 }
 
 class Purchase {
@@ -54,6 +117,7 @@ class Purchase {
 		int PurchaseTest = userPurchaseAmount % LOTTO_PRICE;
 
 		if (!(PurchaseTest == 0)) {
+
 			throw new IllegalArgumentException("[ERROR] 로또는 1000원 단위로 구매 가능합니다.");
 		}
 		return userPurchaseAmount / LOTTO_PRICE;
@@ -80,8 +144,40 @@ class WinningNumber {
 
 	void bonusNumberTest(int bonusNumber) {
 		if (winningNumberTest.contains(bonusNumber)) {
+
 			throw new IllegalArgumentException("[ERROR] 보너스 번호가 당첨 번호에 포함되어 있습니다.");
 		}
 	}
 
 }
+
+class Hit {
+	int hitCount(List<Integer> lottoNumber, List<Integer> winningNumber) {
+		int hitCount = 0;
+		for (int i = 0; i < lottoNumber.size(); i++) {
+			if (winningNumber.contains(lottoNumber.get(i))) {
+				++hitCount;
+			}
+		}
+		return hitCount;
+	}
+
+	List<String> winningHitList(List<List<Integer>> lottoList, List<Integer> winningNumber, int bonusNumber) {
+		List<String> winningHitList = new ArrayList<>();
+		for (int i = 0; i < lottoList.size(); i++) {
+			int hitCount = hitCount(lottoList.get(i), winningNumber);
+
+			if (hitCount == 5 && lottoList.get(i).contains(bonusNumber)) {
+				winningHitList.add(hitCount + "개 일치, 보너스 볼 일치");
+				continue;
+			}
+
+			winningHitList.add(hitCount + "개 일치");
+		}
+		return winningHitList;
+	}
+
+}
+
+
+
