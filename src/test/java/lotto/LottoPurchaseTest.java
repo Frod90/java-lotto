14f88994b23b72accb6800731ee253038fcc37eb
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-09-25 17:09:58 +0900

    Feat: LottoPurchase의 구매 횟수 계산 테스트 코드 구현
    LottoPurchase의 로또 구매 횟수가 정확하게 계산되는지 확인하는 테스트 코드 구현

--- src/test/java/lotto/LottoPurchaseTest.java
+++ src/test/java/lotto/LottoPurchaseTest.java
@@ -1,21 +1,18 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.NoSuchElementException;
+
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
 
 public class LottoPurchaseTest {
 	@DisplayName("구매 금액에 숫자 이외의 것을 넣으면 오류 발생")
 	@Test
 	void inputPriceByString() {
 		LottoPurchase lottoPurchase = new LottoPurchase();
-		assertThatThrownBy(() -> lottoPurchase.purchaseTime("1000j") )
-			.isInstanceOf(NoSuchElementException.class);
+		assertThatThrownBy(() -> lottoPurchase.purchaseTime("1000j")).isInstanceOf(NoSuchElementException.class);
 
 	}
 
@@ -23,10 +20,14 @@ public class LottoPurchaseTest {
 	@Test
 	void inputPriceByUnit() {
 		LottoPurchase lottoPurchase = new LottoPurchase();
-		assertThatThrownBy(() -> lottoPurchase.purchaseTime("5500"))
-			.isInstanceOf(IllegalArgumentException.class);
+		assertThatThrownBy(() -> lottoPurchase.purchaseTime("5500")).isInstanceOf(IllegalArgumentException.class);
 	}
 
-
+	@DisplayName("기입한 금액에 맞는 횟수만큼 로또 개수가 계산되는지 확인한다.")
+	@Test
+	void purchaseTimeCheck() {
+		LottoPurchase lottoPurchase = new LottoPurchase();
+		assertThat(lottoPurchase.purchaseTime("8000")).isEqualTo(8);
+	}
 
 }
