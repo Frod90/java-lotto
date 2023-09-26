8f2136d9656656a49393dfb19cca27eb22bb3125
Frod90 <142146516+Frod90@users.noreply.github.com>
2023-09-26 14:22:45 +0900

    Refactor: LottoPurchase 생성자를 메서드 밖으로 이동
    메서드 내에 LottoPurchase 생성자가 중복 기입되어 있어서 메서드 밖으로 이동

--- src/test/java/lotto/LottoPurchaseTest.java
+++ src/test/java/lotto/LottoPurchaseTest.java
@@ -8,25 +8,23 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class LottoPurchaseTest {
+	LottoPurchase lottoPurchase = new LottoPurchase();
+
 	@DisplayName("구매 금액에 숫자 이외의 것을 넣으면 오류 발생")
 	@Test
 	void inputPriceByString() {
-		LottoPurchase lottoPurchase = new LottoPurchase();
 		assertThatThrownBy(() -> lottoPurchase.purchaseTime("1000j")).isInstanceOf(NoSuchElementException.class);
-
 	}
 
 	@DisplayName("로또 구매 금액이 1000원 단위가 아니면 예외가 발생한다.")
 	@Test
 	void inputPriceByUnit() {
-		LottoPurchase lottoPurchase = new LottoPurchase();
 		assertThatThrownBy(() -> lottoPurchase.purchaseTime("5500")).isInstanceOf(IllegalArgumentException.class);
 	}
 
 	@DisplayName("기입한 금액에 맞는 횟수만큼 로또 개수가 계산되는지 확인한다.")
 	@Test
 	void purchaseTimeCheck() {
-		LottoPurchase lottoPurchase = new LottoPurchase();
 		assertThat(lottoPurchase.purchaseTime("8000")).isEqualTo(8);
 	}
 
